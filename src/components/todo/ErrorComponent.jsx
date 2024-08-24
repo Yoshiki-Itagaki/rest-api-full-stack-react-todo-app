df07746638a874e1f9645940fed27a7505c1c664
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-08-24 23:59:57 +0900

    Refactored React components to separate JavaScript modules

---
+++ src/components/todo/ErrorComponent.jsx
@@ -0,0 +1,10 @@
+export default function ErrorComponent() {
+    return (
+        <div className='ErrorComponent'>
+            <h1>We are working that out really hard!</h1>
+            <div>
+                Apoligies for the 404. Reach out to our team at ABC=DEF=GHIJ.
+            </div>
+        </div>
+    )
+}
---
+++ src/components/todo/WelcomeComponent.jsx
@@ -0,0 +1,19 @@
+import { Link, useParams } from "react-router-dom";
+
+export default function WelcomeComponent() {
+
+    const {username} = useParams();
+
+    console.log(username);
+
+    return (
+        <div> 
+            <h1>Welcome {username}</h1>            
+            <div className='WelcomeComponent'>
+                Manage Your Todos - <Link to='/todos'>Go Ahead</Link>
+            </div>
+
+        </div>
+       
+    )
+}
