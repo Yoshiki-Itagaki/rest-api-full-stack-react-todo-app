9d702b5e4412e4675bbb1f137e3c31fc1e7f6224
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-01 19:50:03 +0900

    Added validation to Todo React component using Formik

--- src/components/TodoComponent.jsx
+++ src/components/TodoComponent.jsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 import { retrieveTodoApi } from "./todo/api/TodoApiService";
 import { useAuth } from "./todo/security/AuthContext";
 import { useEffect, useState } from "react";
-import { Field, Form, Formik } from "formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
 
 export default function TodoComponent() {
 
@@ -35,17 +35,58 @@ export default function TodoComponent() {
         console.log(values);
     }
 
+    function validate(values){
+        let errors = {
+            // description: 'Enter a valid description.',
+            // targetDate: 'Enter a valid target date.',
+        }
+
+        if(values.description.length<5){
+            errors.description = 'Enter at least 5 characters.'
+        }
+
+        if(!values.targetDate){
+            errors.targetDate = 'Enter a target date.'
+        }
+        if(new Date(values.targetDate) <= new Date()){
+            errors.targetDate = 'Enter a date later than today.';
+        }
+
+        console.log('validating', values);
+        console.log('no', values.targetDate <= new Date());
+        console.log(values.targetDate);
+        console.log(new Date());
+
+
+
+        return errors;
+    }
+
     return (
         <div className="container">
             <h1>Enter Todo Details</h1>
             <div>
                 <Formik initialValues={{description, targetDate}} 
                     enableReinitialize={true}
-                    onSubmit={(onSubmit)}
+                    onSubmit={onSubmit}
+                    validate={validate}
+                    validateOnChange={false}
+                    validateOnBlur={false}
                 >
                 {
                     (props) => (
                         <Form>
+
+                            <ErrorMessage 
+                                name="description"
+                                component="div"
+                                className="alert alert-warning"
+                            />
+                            <ErrorMessage 
+                                name="targetDate"
+                                component="div"
+                                className="alert alert-warning"
+                            />
                             <fieldset className="form-group">
                                 <label>Description</label>
                                 <Field type="text" className="form-control" name="description"/>
