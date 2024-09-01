46721d7ea401b5afd3f2659c7e696caa5a02fa37
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-01 19:29:28 +0900

    Added Formik and Moment libraries to display Todo React component

--- src/components/TodoComponent.jsx
+++ src/components/TodoComponent.jsx
@@ -2,15 +2,15 @@ import { useParams } from "react-router-dom";
 import { retrieveTodoApi } from "./todo/api/TodoApiService";
 import { useAuth } from "./todo/security/AuthContext";
 import { useEffect, useState } from "react";
+import { Field, Form, Formik } from "formik";
 
 export default function TodoComponent() {
 
     const {id} = useParams();
     const [description, setDescription] = useState('');
+    const [targetDate, setTargetDate] = useState(null);
     const authContext = useAuth();
     const username = authContext.username;
-
-    console.log(authContext);
 
     useEffect(() => {
         retrieveTodos();
@@ -19,8 +19,11 @@ export default function TodoComponent() {
     function retrieveTodos(){
         retrieveTodoApi(username, id)
             .then(response => {
-                console.log(response);
+                console.log(response.data);
                 setDescription(response.data.description);
+                setTargetDate(response.data.targetDate);
+                console.log(description);
+                console.log(targetDate);
             })
             .catch(error => {
 
@@ -28,11 +31,36 @@ export default function TodoComponent() {
 
     }
 
+    function onSubmit(values){
+        console.log(values);
+    }
+
     return (
         <div className="container">
             <h1>Enter Todo Details</h1>
             <div>
-                description: {description}
+                <Formik initialValues={{description, targetDate}} 
+                    enableReinitialize={true}
+                    onSubmit={(onSubmit)}
+                >
+                {
+                    (props) => (
+                        <Form>
+                            <fieldset className="form-group">
+                                <label>Description</label>
+                                <Field type="text" className="form-control" name="description"/>
+                            </fieldset>
+                            <fieldset className="form-group">
+                                <label>Target Date</label>
+                                <Field type="date" className="form-control" name="targetDate"/>
+                            </fieldset>
+                            <div>
+                                <button className="btn btn-success" type="submit">Save</button>
+                            </div>
+                        </Form>
+                    )
+                }
+                </Formik>
             </div>
         </div>
     )
--- src/components/todo/HeaderComponent.jsx
+++ src/components/todo/HeaderComponent.jsx
@@ -26,7 +26,7 @@ export default function HeaderComponent() {
                             </li>                               
                             <li className='nav-item fs-5'>
                                 {isAuthenticated &&
-                                    <Link className='nav-link' to='/todo'>Todos</Link>
+                                    <Link className='nav-link' to='/todos'>Todos</Link>
                                 }
                             </li>
                         </ul>
